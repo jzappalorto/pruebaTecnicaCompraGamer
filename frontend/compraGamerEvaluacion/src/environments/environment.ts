bd426770f00251e35938444706c0c9cd1fbdc261
Juan Manuel Zappalorto <85179583+jzappalorto@users.noreply.github.com>
2025-11-14 17:32:26 -0300

    docu
    se documenta en el file de environments las URL de la aplicacion para que llame al backend del docker

--- frontend/compraGamerEvaluacion/src/environments/environment.ts
+++ frontend/compraGamerEvaluacion/src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'http://host.docker.internal:8080/api'
 };
+//host.docker.internal
 //el puerto 8080 es del backend del docker, 
 //el puerto 5000 es el que levanta el VS
 //export const environment = {
